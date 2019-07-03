ab43f786dde9286b9df6ec1ada04fa0c1da3da6d
Vehikl Team <go@vehikl.com>
2019-07-03 14:08:30 -0400

    remove our code

--- bowling.js
+++ bowling.js
@@ -12,25 +12,4 @@ export class Bowling {
     score() {
         return this.total;
     }
-
-    addFrame() {
-        const frame = {
-            firstRoll: null,
-            secondRoll: null,
-        }
-        this.frames.push(frame);
-        return frame;
-    }
-
-    needsNewFrame() {
-        const frame = this.frames[this.frames.length - 1];
-        return frame.secondRoll !== null || frame.firstRoll === 10;
-    }
-
-    getCurrentFrame() {
-        if (this.needsNewFrame()) {
-            return this.addFrame();
-        }
-        return this.frames[this.frames.length - 1];
-    }
 }
